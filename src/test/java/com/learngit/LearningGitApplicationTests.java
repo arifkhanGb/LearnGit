038bd2ef46dee3108c461d9b77ee80423a2d03c0
arif90526@gmail.com <arif90526@gmail.com>
2024-02-10 22:26:18 -0800

    Initial commit

---
+++ src/test/java/com/learngit/LearningGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learngit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
